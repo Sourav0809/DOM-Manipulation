9d816f4b96331a9155c1e2656fc45da69b073c56
Sourav0809 <spathak7431@gmail.com>
2023-05-06 21:13:09 +0530

    sec commit

--- Task4/script.js
+++ Task4/script.js
@@ -1,4 +1,8 @@
 let items = document.querySelectorAll(".list-group-item")
 console.log(items)
-items[1].style.backgroundColor = "green"
-items[2].style.visibility = "hidden"
+items[1].style.color = "green"
+for (let i = 0; i < items.length; i++) {
+    if (i % 2 == 0) {
+        items[i].style.backgroundColor = "green"
+    }
+}
