a9c91cb599734a7e50b25fcfd59e34b8cba9b77f
Sourav0809 <spathak7431@gmail.com>
2023-05-06 18:11:14 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const title = document.getElementById("header-title")
+title.innerText = "I will do That"
+// console.log(title.innerText)
+title.style.border = "2px solid white"
+title.style.padding = "10px"
+
+const additem = document.querySelector(".new_title")
+additem.style.color = "red"
