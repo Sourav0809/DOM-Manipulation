904a314cb450a784bb25a1448b16c62125afd175
Sourav0809 <spathak7431@gmail.com>
2023-05-07 13:55:01 +0530

    5. firstelementchild

--- Task5/script.js
+++ Task5/script.js
@@ -1,6 +1,6 @@
 const items = document.querySelector("#items")
 
-// ParentElement
+// 1.ParentElement
 // items.parentElement.style.border = "thick solid blue"
 
 
@@ -8,7 +8,19 @@ const items = document.querySelector("#items")
 // ChildNodes
 // console.log(items.firstElementChild)
 // console.log(items.lastElementChild)
+
+// 2.LastElement Child
+
 // items.lastElementChild.style.color = "red"
 
-console.log(items.lastChild)
+
+// 3. last child 
+// console.log(items.lastChild)
 // we got the empty space
+
+
+// 4.firstelementchild
+
+console.log(items.firstElementChild)
+items.firstElementChild.textContent = " Hey Whats Up !!"
+// it is return the first element
