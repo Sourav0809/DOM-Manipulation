7e8a7e1f1674f718fdd56cf71cf9f5b9bc697186
Sourav0809 <spathak7431@gmail.com>
2023-05-09 12:05:29 +0530

    no changes

--- Task9/script.js
+++ Task9/script.js
@@ -1,3 +1,4 @@
+
 const form = document.querySelector(".form")
 const FormItem = document.querySelector(".flexitems")
 
@@ -74,81 +75,6 @@ function search(e) {
     }
 }
 
-const form = document.querySelector(".form")
-const FormItem = document.querySelector(".flexitems")
-
-
-
-// addItem 
-form.addEventListener("submit", addItems)
-
-//addItem Function 
-
-function addItems(e) {
-    e.preventDefault()
-
-
-    let dummy = document.querySelector(".noelement")
-    if (dummy != null) {
-        dummy.remove()
-    }
-
-
-    const InputVal = document.getElementById("sec-form-input").value
-
-    const li = document.createElement("li")
-    li.className = "listitem"
-
-    const span = document.createElement("span")
-    span.appendChild(document.createTextNode(InputVal))
-    li.appendChild(span)
-
-    // Adding the Button
-    const deleteBtn = document.createElement("button")
-    deleteBtn.className = "secBtn"
-    deleteBtn.appendChild(document.createTextNode("X"))
-    li.appendChild(deleteBtn)
-    FormItem.appendChild(li)
-}
-
-
-// delete item
-
-FormItem.addEventListener("click", RemoveItem)
-
-function RemoveItem(e) {
-    if (e.target.classList.contains("secBtn")) {
-        if (confirm("Are You Sure ?? ")) {
-            var li = e.target.parentElement
-            FormItem.removeChild(li)
-        }
-        // check if no elemet is present 
-
-        if (FormItem.children.length == 0) {
-            FormItem.innerHTML += " <h2 class='noelement'> No item Present ADD NOW !! </h2>"
-
-        }
-    }
-}
-
-
-// filter 
-
-
-let filteritem = document.querySelector("#input")
-
-filteritem.addEventListener("keyup", search)
-
-function search(e) {
-    for (let i = 0; i < FormItem.children.length; i++) {
-        if (FormItem.children[i].children[0].textContent.toLowerCase().includes(filter.value.toLowerCase())) {
-            FormItem.children[i].style.display = "flex"
-        }
-        else {
-            FormItem.children[i].style.display = "none"
-        }
-    }
-}
 
 
 
